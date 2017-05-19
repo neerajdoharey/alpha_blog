e22ac91c4d6aca082f8d19ed6eced5eecf37ce16
Neeraj Doharey <neerajlivewire@gmail.com>
2017-05-19 16:29:20 +0530

    Adds assocciation between User and Articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-
+  has_many :articles
+  before_save { self.email = email.downcase}
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-  
+
   VALID_EMAIL =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensetive: false},
   format: {with: VALID_EMAIL}
